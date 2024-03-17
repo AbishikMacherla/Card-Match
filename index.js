b7a10bde3917f916d85fdc9d160c1c4e006a58ea
AbishikMacherla <40594078@live.napier.ac.uk>
2024-03-17 18:02:37 +0000

    Update in JavaScript
    Found a solution for grouping the card number and suite. I have grouped the card number & suite and pushed into a array which will append the each card everytime it generates a new pattern

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 function arrayOfNumbers(numbers, kind) {
-for (let index = 0; index < 9; index++)
-{
-    var random = Math.random();
-    let numbers_array = numbers[(Math.floor(random * numbers.length))];
-    let kind_array = kind[(Math.floor(random * kind.length))];
-    let random_deck = {numbers_array:kind_array};
-    console.log(random_deck);
+    let cards = [];
+    for (let index = 0; index < 9; index++) {
+        var random = Math.random();
+        var number = numbers[(Math.floor(random * numbers.length))];
+        var suite = kind[(Math.floor(random * kind.length))];
+        cards.push({card_number:number,card_suite:suite});
+    }
+    return cards;
 }
-}
-
-//let user_input = prompt("Please enter the any card value:");
 
 let card_values = ["A","2","3","4","5","6","7","8","9","10","J","K","Q"];
 let suites = ["Diamond","Heart","Spades","Clover"];
 
-arrayOfNumbers(card_values, suites);
+let cards = arrayOfNumbers(card_values, suites);
+
+for (let index = 0; index < cards.length; index++) {
+    console.log(cards[index]);
+}
