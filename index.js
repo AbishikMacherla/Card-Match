c5c61f255fc65acd69c7900654ce7a1fe16333b3
AbishikMacherla <40594078@live.napier.ac.uk>
2024-03-22 21:12:10 +0000

    Remove buttons in HTML and Updated in JavaScript
    Because of hard coding too many button s, I found a dynamic way of creating the buttons in JavaScript.

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-//function array_of_numbers(numbers, kind) {
-//    let cards = [];
-//    for (let index = 0; index < 9; index++) {
-//        var random = Math.random();
-//        var number = numbers[(Math.floor(random * numbers.length))];
-//        var suite = kind[(Math.floor(random * kind.length))];
-//        cards.push({card_suite:suite,card_number:number});
-//    }
-//    return cards;
-//}
+const suits = ["Diamond", "Heart", "Spades", "Clover"];
+const values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "Q"];
+
+const table = document.querySelector('table');
+
+for (let value of values) {
+    const row = document.createElement('tr');
+
+    for (let suit of suits) {
+        const button = document.createElement('button');
+        button.textContent = value;
+        button.id = `${suit}_${value}`;
+        button.addEventListener('click', () => ValidateUserInput(button.value));
+        row.appendChild(button);
+    }
+    table.appendChild(row);
+}
+
 function ArrayOfNumbers(numbers, kind) {
     let cards = [];
     let card = new Set();
@@ -37,25 +45,25 @@ function SumOfTheCardValues(cards) {
 }
 
 function ValidateUserInput(buttonValue) {
-    const buttons = document.querySelectorAll('button');
     console.log(buttonValue);
     let [suite, number] = buttonValue.split("_");
     console.log(suite);
     console.log(number);
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function() {
-            //console.log(buttons[i].id);
-        });
-    }
 }
 
 let card_values = ["A","2","3","4","5","6","7","8","9","10","J","K","Q"];
 let suites = ["Diamond","Heart","Spades","Clover"];
 
 let cards = ArrayOfNumbers(card_values, suites);
-SumOfTheCardValues(cards);
-ValidateUserInput();
-
 for (let index = 0; index < cards.length; index++) {
     console.log(cards[index]);
+}
+
+SumOfTheCardValues(cards);
+
+const buttons = document.querySelectorAll('button');
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+        console.log(buttons[i].id);
+    });
 }
