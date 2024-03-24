ef96da8a1b6a6577fb3d594b3b317a0f6f78da65
AbishikMacherla <40594078@live.napier.ac.uk>
2024-03-24 16:51:16 +0000

    -

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-function CreateButtons(values,suites) {
-    const table = document.querySelector('table');
+const suits = ["Diamond", "Heart", "Spades", "Clover"];
+const values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "Q"];
 
-    for (let value of values) {
-        const row = document.createElement('tr');
-        for (let suit of suites) {
-            const tabledata= document.createElement('td');
-            const button = document.createElement('button');
-            button.textContent = value;
-            button.classList.add("btn-primary");
-            button.classList.add("card-number-btn");
-            button.id = `${suit}_${value}`;
-            button.addEventListener('click', () => ValidateUserInput(button.value));
-            tabledata.appendChild(button);
-            tabledata.classList.add("card-number-td");
-            row.appendChild(tabledata);
-        }
-        table.appendChild(row);
+const table = document.querySelector('table');
+
+for (let value of values) {
+    const row = document.createElement('tr');
+
+    for (let suit of suits) {
+        const button = document.createElement('button');
+        button.textContent = value;
+        button.id = `${suit}_${value}`;
+        button.addEventListener('click', () => ValidateUserInput(button.value));
+        row.appendChild(button);
     }
+    table.appendChild(row);
 }
 
 function CreateMatrix() {
@@ -63,7 +60,7 @@ function ArrayOfNumbers(numbers, kind) {
 function SumOfTheCardValues(cards) {
     let sum = 0;
     for (let card of cards) {
-        let card_value = values.indexOf(card.card_number) + 1;
+        let card_value = card_values.indexOf(card.card_number) + 1;
         sum = sum + card_value;
     }
     document.getElementById("sum_of_cards").innerHTML = `${sum}`;
@@ -72,15 +69,15 @@ function SumOfTheCardValues(cards) {
 
 function ValidateUserInput(buttonValue) {
     console.log(buttonValue);
+    let [suite, number] = buttonValue.split("_");
+    console.log(suite);
+    console.log(number);
 }
 
+let card_values = ["A","2","3","4","5","6","7","8","9","10","J","K","Q"];
+let suites = ["Diamond","Heart","Spades","Clover"];
 
-const values = ["A","2","3","4","5","6","7","8","9","10","J","K","Q"];
-const suites = ["Diamond","Heart","Spades","Clover"];
-
-CreateButtons(values,suites);
-
-let cards = ArrayOfNumbers(values, suites);
+let cards = ArrayOfNumbers(card_values, suites);
 for (let card of cards) {
     console.log(card);
 }
